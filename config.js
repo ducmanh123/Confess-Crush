355a73621562d8381d06e8d1623e485c6f0795fb
ducmanh123 <96278839+ducmanh123@users.noreply.github.com>
2021-12-17 12:11:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "yeu em hehe",
+    introTitle: 'Nguyễn Khánh Linh',
+    introDesc: `chồng xin lỗi vợ về nhiều câu chồng nói
+    do chồng bực quá nhiều khi khồng kiềm được
+    xin lỗi vợ nhiều`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'tha lỗi cho chồng nha😁🥰',
+    desc: 'đọc được thì ib cho chồng  nha ',
+    btnYes: 'Tha lỗi <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu tui <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'tha lỗi điiiiiiii <33333333',
+    mess: 'Mình biết mà 🥰. Yêu emmm nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, chồng qua đón đi chơi nha.',
+    btnAccept: 'nghe lun <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //https://www.facebook.com/messages/t/100058994515499
 }
